b26103102ef2406a996621e4036881b6edeac034
Dhanya-bhat <53563154+Dhanya-bhat@users.noreply.github.com>
2025-07-10 10:00:57 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -2,12 +2,7 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/run', methods=['GET'])
+@app.route("/run")
 def run():
-    return jsonify({
-        "message": "Hello from Python on Render!",
-        "status": "Success"
-    })
+    return jsonify({"message": "Hello from Render!"})
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
